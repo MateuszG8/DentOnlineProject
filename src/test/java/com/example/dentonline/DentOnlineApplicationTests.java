34ea429615ddf87f63f0f31205656520e0a194ef
MateuszG8 <mateusz.guzowski8@gmail.com>
2023-11-13 17:10:13 +0100

    first commit - Register/Login/Auth

---
+++ src/test/java/com/example/dentonline/DentOnlineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.dentonline;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DentOnlineApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
